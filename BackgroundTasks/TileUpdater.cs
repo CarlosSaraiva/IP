d5198114fb87a684e041a9c3b7ab612e50268244
Carlos Saraiva <carlos.saraiva@msn.com>
2015-08-22 00:35:05 -0300

    General refactoring

---
+++ BackgroundTasks/TileUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTasks
+{
+    public sealed class TileUpdaterTask:IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+            //To DO: Executar a tarefa de fundo
+            deferral.Complete();
+        }
+    }
+}
---
+++ IP/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            StartWebRequest();
+        }
+
+        private void StartWebRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest) HttpWebRequest.Create("http://myexternalip.com/raw");
+            request.BeginGetResponse(new AsyncCallback(FinishWebRequest), request);
+
+        }
+
+        private async void FinishWebRequest(IAsyncResult result)
+        {
+            HttpWebResponse response = (result.AsyncState as HttpWebRequest).EndGetResponse(result) as HttpWebResponse;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                StreamReader stream = new StreamReader(response.GetResponseStream());
+                var ip = stream.ReadToEnd();
+                textBox.Text = ip;
+                UpdateTile(ip);
+            });
+        }
+
+        private static void UpdateTile(string infoString)
+        {
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.EnableNotificationQueue(true);
+            updater.Clear();
+            Windows.Data.Xml.Dom.XmlDocument tileXml =
+                TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Text05);
+            tileXml.GetElementsByTagName("text")[0].InnerText = infoString;
+            updater.Update(new TileNotification(tileXml));
+
+        }
+    }
+}
